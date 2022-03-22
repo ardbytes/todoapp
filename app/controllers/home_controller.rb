68a2637d9c397a5524796579a9c416b789e0ffa3
Arvind <ardsrk@gmail.com>
2022-03-22 19:43:17 +0530

    add landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :only => [:index]
 
   def index
     render :plain => "Hello #{current_user.email.split("@").first}"
+  end
+
+  def welcome
   end
 end
